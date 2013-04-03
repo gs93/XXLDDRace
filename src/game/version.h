4ba1ad72fb45ba988e45bb9e19dba83858045967
XXLTomate <XXLTomate@v93.eu>
2013-04-04 00:31:08 +0200

    added 64p to the server string

--- src/game/version.h
+++ src/game/version.h
@@ -5,7 +5,7 @@
 #include "generated/nethash.cpp"
 #define DDRACE_VERSION "0.6, 1.15a"
 #define XXL_VERSION "1.9a"
-#define GAME_VERSION DDRACE_VERSION ", " XXL_VERSION
+#define GAME_VERSION DDRACE_VERSION ", " XXL_VERSION ", 64p"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '1', 0};
 #endif
